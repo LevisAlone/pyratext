ef1627cda2ec6a1eccfcb5c43bb0d265fcb8b88c
LevisAlone <LeviMacythegamer@yahoo.com>
2018-10-25 01:47:41 -0700

    Created branch

---
+++ pyratext/pyratext/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ptext
+{
+    class Program
+    {
+        private static string GeneratePyramidText(string args)
+        {
+            string s = "";
+            string allChars = "";
+            foreach (char c in args)
+            {
+                allChars += c;
+                if (c != ' ')
+                    s += $"{allChars}\n";
+            }
+            while (allChars.Length > 1)
+            {
+                allChars = allChars.Substring(0, allChars.Length - 1);
+                if (allChars[allChars.Length - 1] != ' ')
+                {
+                    if (allChars.Length == 1)
+                        s += $"{allChars}";
+                    else s += $"{allChars}\n";
+                }
+            }
+            return s;
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new Exception("Args are empty!");
+            }
+            Clipboard.SetText(GeneratePyramidText(string.Join(" ", args)));
+            Console.WriteLine("Copied to clipboard.");
+        }
+    }
+}
